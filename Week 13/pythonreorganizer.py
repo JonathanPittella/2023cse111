39cccc977116e063a00b6822b0c9ab8a42af7a79
JonathanPittella <jonathanpittella22@gmail.com>
2023-12-08 21:58:46 -0300

    w13 update

--- Week 13/pythonreorganizer.py
+++ Week 13/pythonreorganizer.py
@@ -30,9 +30,11 @@ def main():
 
     root.mainloop()
 
+
 def populate_main_window(frm_main):
+    global ent_age, result_text  # Declare ent_age and result_text as global
     lbl_csv_file = Label(frm_main, text=f"CSV File: {csv_file_path}")
-    ent_age = IntEntry(frm_main, width=4)  # Add this line to define ent_age
+    ent_age = IntEntry(frm_main, width=4)
     lbl_age_units = Label(frm_main, text="(Enter Category Number)")
     lbl_rates = Label(frm_main, text=":")
     lbl_slow = Label(frm_main, width=3)
@@ -46,7 +48,7 @@ def populate_main_window(frm_main):
     result_text.config(yscrollcommand=result_text_scrollbar.set)
 
     lbl_csv_file.grid(row=0, column=0, columnspan=4, padx=3, pady=3)
-    ent_age.grid(row=1, column=0, padx=3, pady=3)  # Update this line
+    ent_age.grid(row=1, column=0, padx=3, pady=3)
     lbl_age_units.grid(row=1, column=1, padx=0, pady=3)
     lbl_rates.grid(row=2, column=0, padx=(30, 3), pady=3)
     lbl_slow.grid(row=2, column=1, padx=3, pady=3)
@@ -57,61 +59,57 @@ def populate_main_window(frm_main):
     result_text.grid(row=4, column=0, columnspan=4, padx=3, pady=3, sticky="nsew")
     result_text_scrollbar.grid(row=4, column=4, pady=3, sticky="ns")
 
-    # Display categories count in the Text widget
-    result_text.delete("1.0", tk.END)  # Clear previous content
+    ent_age.bind("<KeyRelease>", show_products)
+    btn_clear.config(command=clear)
+
+    result_text.delete("1.0", tk.END)
     for i, (category, count) in enumerate(categories_count.items(), start=1):
         result_text.insert(tk.END, f"{i}. {category}: {count} products\n")
 
-    def show_products(event=None):  # Modify the function signature
-        try:
-            selected_category_number = ent_age.get()
-            if 1 <= selected_category_number <= len(categories_count):
-                selected_category = list(categories_count.keys())[selected_category_number - 1]
-                products = get_products_for_category(csv_file_path, selected_category)
-                result_text.delete("1.0", tk.END)  # Clear previous content
-                result_text.insert(tk.END, f"Products in Category '{selected_category}':\n")
-                for product in products:
-                    result_text.insert(tk.END, f"{product}\n")
-            else:
-                result_text.delete("1.0", tk.END)  # Clear previous content
-                result_text.insert(tk.END, f"Invalid category number: {selected_category_number}\n")
-        except ValueError:
-            lbl_fast.config(text="")
+def show_products(event=None):
+    try:
+        selected_category_number = ent_age.get()
+        if 1 <= selected_category_number <= len(categories_count):
+            selected_category = list(categories_count.keys())[selected_category_number - 1]
+            products = get_products_for_category(csv_file_path, selected_category)
+            result_text.delete("1.0", tk.END)  # Clear previous content
+            result_text.insert(tk.END, f"Products in Category '{selected_category}':\n")
+            for product in products:
+                result_text.insert(tk.END, f"{product}\n")
+        else:
+            result_text.delete("1.0", tk.END)  # Clear previous content
+            result_text.insert(tk.END, f"Invalid category number: {selected_category_number}\n")
+    except ValueError:
+        pass
 
-    # Rest of your code...
+def clear():
+    global ent_age, result_text  # Add this line to declare ent_age as global
+    ent_age.clear()
+    result_text.delete("1.0", tk.END) 
+    ent_age.focus()
 
-    # Bind the show_products function to the KeyRelease event of ent_age
-    ent_age.bind("<KeyRelease>", show_products)
-
-
-    def clear():
-        ent_age.clear()
-        lbl_slow.config(text="")
-        lbl_fast.config(text="")
-        # Repopulate the original list of categories
-        result_text.delete("1.0", tk.END)  # Clear previous content
-        for i, (category, count) in enumerate(categories_count.items(), start=1):
-            result_text.insert(tk.END, f"{i}. {category}: {count} products\n")
-        ent_age.focus()
-
+    # Repopulate the original list of categories
+    result_text.delete("1.0", tk.END) 
+    for i, (category, count) in enumerate(categories_count.items(), start=1):
+        result_text.insert(tk.END, f"{i}. {category}: {count} products\n")
 
 def analyze_csv(csv_file_path):
     with open(csv_file_path, 'r') as file:
         csv_reader = csv.reader(file)
-        next(csv_reader)  # Skip header
+        next(csv_reader)
         first_word_column_index = 0
         categories_count = Counter(row[first_word_column_index].split()[0] for row in csv_reader)
 
-        return categories_count
+    return categories_count
 
 def get_products_for_category(csv_file_path, category):
     with open(csv_file_path, 'r') as file:
         csv_reader = csv.reader(file)
-        next(csv_reader)  # Skip header
+        next(csv_reader)  
         first_word_column_index = 0
         products = [row[first_word_column_index] for row in csv_reader if row[first_word_column_index].split()[0] == category]
 
-        return products
+    return products
 
 if __name__ == "__main__":
     main()
